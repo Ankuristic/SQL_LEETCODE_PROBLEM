e666f3be855a7a7d823ce7155e629883286a1443
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-03-28 08:25:03 +0530

    third commit

--- Medium/winingCandidate.sql
+++ Medium/winingCandidate.sql
@@ -39,3 +39,29 @@ You may assume there is no tie, in other words there will be at most one winning
  select name from candidate where id in (select candidate id from vote group by candidateid 
  order by count(id)
  )
+
+
+
+ id is the auto-increment primary key,
+CandidateId is the id appeared in Candidate table.
+Write a sql to find the name of the winning candidate, the above example will return the winner B.
++------+
+| Name |
++------+
+| B    |
++------+
+Notes:
+You may assume there is no tie, in other words there will be only one winning candidate
+
+
+
+
+WITH cte AS (
+SELECT c.id,c.Name, COUNT(c.id) AS cnt FROM Candidate c INNER JOIN Vote v ON c.id=v.CandidateId GROUP BY c.id,c.Name
+)
+SELECT Top 1 Name FROM cte ORDER BY cnt DESC
+
+WITH cte AS (
+SELECT c.id,c.Name, COUNT(c.id) AS cnt FROM Candidate c INNER JOIN Vote v ON c.id=v.CandidateId GROUP BY c.id,c.Name
+)
+SELECT Name FROM cte WHERE (cnt) = (SELECT MAX(cnt) FROM cte)
